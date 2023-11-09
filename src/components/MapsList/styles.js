d1874ded0ccdadf20f631227c9b908a392bc1c29
gabriel feitosa <contato.gabrielfeitosa@gmail.com>
2023-11-09 18:17:45 -0300

    adjustments on maps list

--- src/components/MapsList/styles.js
+++ src/components/MapsList/styles.js
@@ -1,26 +1,37 @@
 import styled from "styled-components/native"
 
 export const List = styled.ScrollView`
-  paddingTop: 18px;
+  paddingVertical: 16px;
   width: 100%;
 `
 
 export const Container = styled.View`
-  width: 100%;
+  width: 90%;
+
+  align-self: center;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
+  margin-bottom: 16px;
+`
+
+export const BgImage = styled.ImageBackground`
+  width: 100%;
+  height: 160px;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 16px;
 `
 
 export const ButtonMap = styled.TouchableOpacity.attrs({
   activeOpacity: 0.7
 })`
-  width: 90%;
-  height: 150px;
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
   align-items: center;
   justify-content: center;
-  border-radius: 10px;
-  margin-bottom: 24px;
-  background-color: gray;
+  background-color: rgba(0, 0, 0, 0.4);
 `
 
 export const BtnTitle = styled.Text`
