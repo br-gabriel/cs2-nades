9ebac0fd4aec98a96fd09b3fd925e3d6efaac95e
gabriel feitosa <contato.gabrielfeitosa@gmail.com>
2023-11-09 18:17:06 -0300

    Added background image on buttons

--- src/components/MapsList/index.jsx
+++ src/components/MapsList/index.jsx
@@ -1,40 +1,88 @@
-import { List, ButtonMap, BtnTitle, Container } from "./styles"
+import { List, BgImage, ButtonMap, BtnTitle, Container } from "./styles"
 
 export function MapsList() {
   return (
     <List>
       <Container>
-        <ButtonMap>
-          <BtnTitle>Mirage</BtnTitle>
-        </ButtonMap>
+        <BgImage 
+          source={require('../../../assets/maps-thumbs/mirage4.jpg')}
+          borderRadius={10}
+          resizeMode="cover"
+        >
+          <ButtonMap>
+            <BtnTitle>Mirage</BtnTitle>
+          </ButtonMap>
+        </BgImage>
 
-        <ButtonMap>
-          <BtnTitle>Inferno</BtnTitle>
-        </ButtonMap>
+        <BgImage 
+          source={require('../../../assets/maps-thumbs/inferno8.jpg')}
+          borderRadius={10}
+          resizeMode="cover"
+        >
+          <ButtonMap>
+            <BtnTitle>Inferno</BtnTitle>
+          </ButtonMap>
+        </BgImage>
 
-        <ButtonMap>
-          <BtnTitle>Overpass</BtnTitle>
-        </ButtonMap>
+        <BgImage 
+          source={require('../../../assets/maps-thumbs/overpass5.jpg')}
+          borderRadius={10}
+          resizeMode="cover"
+        >
+          <ButtonMap>
+            <BtnTitle>Overpass</BtnTitle>
+          </ButtonMap>
+        </BgImage>
 
-        <ButtonMap>
-          <BtnTitle>Dust II</BtnTitle>
-        </ButtonMap>
+        <BgImage 
+          source={require('../../../assets/maps-thumbs/dust4.jpg')}
+          borderRadius={10}
+          resizeMode="cover"
+        >
+          <ButtonMap>
+            <BtnTitle>Dust II</BtnTitle>
+          </ButtonMap>
+        </BgImage>
 
-        <ButtonMap>
-          <BtnTitle>Nuke</BtnTitle>
-        </ButtonMap>
+        <BgImage 
+          source={require('../../../assets/maps-thumbs/nuke1.jpg')}
+          borderRadius={10}
+          resizeMode="cover"
+        >
+          <ButtonMap>
+            <BtnTitle>Nuke</BtnTitle>
+          </ButtonMap>
+        </BgImage>
 
-        <ButtonMap>
-          <BtnTitle>Ancient</BtnTitle>
-        </ButtonMap>
+        <BgImage 
+          source={require('../../../assets/maps-thumbs/ancient3.jpg')}
+          borderRadius={10}
+          resizeMode="cover"
+        >
+          <ButtonMap>
+            <BtnTitle>Ancient</BtnTitle>
+          </ButtonMap>
+        </BgImage>
 
-        <ButtonMap>
-          <BtnTitle>Anubis</BtnTitle>
-        </ButtonMap>
+        <BgImage 
+          source={require('../../../assets/maps-thumbs/anubis9.jpg')}
+          borderRadius={10}
+          resizeMode="cover"
+        >
+          <ButtonMap>
+            <BtnTitle>Anubis</BtnTitle>
+          </ButtonMap>
+        </BgImage>
 
-        <ButtonMap>
-          <BtnTitle>Vertigo</BtnTitle>
-        </ButtonMap>
+        <BgImage 
+          source={require('../../../assets/maps-thumbs/vertigo6.jpg')}
+          borderRadius={10}
+          resizeMode="cover"
+        >
+          <ButtonMap>
+            <BtnTitle>Vertigo</BtnTitle>
+          </ButtonMap>
+        </BgImage>
       </Container>
     </List>
   )
