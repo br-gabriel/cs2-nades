c46268d3964b6a315cf3f0cb22829665d428e91a
gabriel feitosa <contato.gabrielfeitosa@gmail.com>
2023-11-12 15:08:04 -0300

    files moved to front-end folder

--- front-end/App.js
+++ front-end/App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Header } from './src/components/Header/index'
+import { MapsList } from './src/components/MapsList';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Header />
+      <MapsList />
     </View>
   );
 }
@@ -14,7 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
