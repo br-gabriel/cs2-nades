b26f222fd46781ee10dc3d452d1af1b3584abdfb
gabriel feitosa <contato.gabrielfeitosa@gmail.com>
2023-11-12 21:06:12 -0300

    added routes on app

--- front-end/App.js
+++ front-end/App.js
@@ -1,23 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { Header } from './src/components/Header/index'
-import { MapsList } from './src/components/MapsList';
-
+import { Routes } from "./src/routes";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <Header />
-      <MapsList />
-    </View>
+    <Routes />
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
-});
+};
