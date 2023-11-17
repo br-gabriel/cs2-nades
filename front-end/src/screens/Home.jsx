c3e75452144b5a3cb1f1739fe55534f0353d1802
gabriel feitosa <contato.gabrielfeitosa@gmail.com>
2023-11-16 21:05:56 -0300

    stylesheet removed

--- front-end/src/screens/Home.jsx
+++ front-end/src/screens/Home.jsx
@@ -1,22 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 import { Header } from '../components/Header/index';
 import { MapsList } from '../components/MapsList/index';
 
 export function Home() {
   return (
-    <View style={styles.container}>
+    <View>
       <StatusBar style="light" />
       <Header />
       <MapsList />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
-});
