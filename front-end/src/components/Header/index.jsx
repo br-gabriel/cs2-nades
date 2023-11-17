825592487c94fdcdfcaf9a533b6b77a499a6e173
gabriel feitosa <contato.gabrielfeitosa@gmail.com>
2023-11-16 21:04:10 -0300

    Added new title

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import { Container, Title } from "./styles"
 export function Header() {
   return (
     <Container>
-      <Title>Mapas</Title>
+      <Title>CS2 Nades</Title>
     </Container>
   )
 }
