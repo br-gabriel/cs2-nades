88b83f35d4e5a576b170b93ff965be8209f869a7
Anderson Assunção <andersnn7@gmail.com>
2023-11-21 01:00:47 -0500

    feat/conexao com mongodb

--- back-end/app.js
+++ back-end/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const app = express();
+const connect = require('./database/connection');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+connect()
 
 module.exports = app;
---
+++ back-end/database/connection.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+async function connect() {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b9hdxul.mongodb.net/?retryWrites=true&w=majority`, {
+        });
+        console.log('Conexão bem-sucedida!');
+    } catch (error) {
+        console.log('Conexão mal-sucedida!');
+    }
+}
+
+module.exports = connect;
