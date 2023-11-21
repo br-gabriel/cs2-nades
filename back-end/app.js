67adb1b6e4fb1fa5fbec841213a5a044ccb3dc60
Anderson Assunção <andersnn7@gmail.com>
2023-11-21 15:16:55 -0500

    feat/schemas de mapa, nades e users

--- back-end/app.js
+++ back-end/app.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const app = express();
 const connect = require('./database/connection');
+const mapsController = require('./controllers/mapController');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -10,5 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 connect()
+
+app.post('/maps/add', mapsController.submitMap);
 
 module.exports = app;
--- back-end/database/connection.js
+++ back-end/database/connection.js
@@ -7,7 +7,7 @@ async function connect() {
         });
         console.log('Conexão bem-sucedida!');
     } catch (error) {
-        console.log('Conexão mal-sucedida!');
+        console.log('Conexão mal-sucedida!' + error);
     }
 }
 
