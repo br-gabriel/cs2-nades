0032a5dd64e843324419d0ee2196493efa981415
gabriel feitosa <contato.gabrielfeitosa@gmail.com>
2023-12-26 23:13:52 -0300

    added a clear button to selected grenades

--- front-end/src/components/BackButton/index.jsx
+++ front-end/src/components/BackButton/index.jsx
@@ -1,23 +1,36 @@
-import { Header, BackBtn, Title } from './styles';
+import { Header, BackBtn, Title, Container, ClearBtn, BtnTitle } from './styles';
 import { useNavigation } from '@react-navigation/native';
 import { Entypo } from '@expo/vector-icons';
+import { useContext } from 'react';
+import { SelectedContext } from '../../context/selectedContext';
+import { Text } from 'react-native';
 
-export function BackButton({title}) {
+export function BackButton({ title }) {
   const { goBack } = useNavigation();
+  const {clickedNade, setClickedNade} = useContext(SelectedContext)
 
-  return(
+  return (
     <Header>
-      <BackBtn onPress={goBack}>
-        <Entypo 
-          name="chevron-small-left"
-          size={30}
-          color={"#fff"}
-        />
-      </BackBtn>
+      <Container>
+        <BackBtn onPress={goBack}>
+          <Entypo
+            name="chevron-small-left"
+            size={30}
+            color={"#fff"}
+          />
+        </BackBtn>
 
-      <Title>
-        {title}
-      </Title>
+        <Title>
+          {title}
+        </Title>
+      </Container>
+      {clickedNade && (
+        <ClearBtn
+          onPress={() => setClickedNade(null)}
+        >
+          <BtnTitle>CLEAR</BtnTitle>
+        </ClearBtn>
+      )}
     </Header>
   );
 };
