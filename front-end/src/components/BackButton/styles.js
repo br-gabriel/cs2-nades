c5dfaab28376616aebac01324f875e8848738535
gabriel feitosa <contato.gabrielfeitosa@gmail.com>
2023-12-26 23:14:16 -0300

    added new components and style adjustment

--- front-end/src/components/BackButton/styles.js
+++ front-end/src/components/BackButton/styles.js
@@ -9,6 +9,11 @@ export const Header = styled.View`
 
   flex-direction: row;
   align-items: center;
+  justify-content: space-between;
+`
+
+export const Container = styled.View`
+  flex-direction: row;
   gap: 12px;
 `
 
@@ -23,4 +28,20 @@ export const BackBtn = styled.TouchableOpacity.attrs({
   activeOpacity: 0.7
 })`
   width: 30px;
+`
+
+export const ClearBtn = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7
+})`
+  background-color: #6E6FFF;
+  border-radius: 5px;
+  margin-right: 7px;
+`
+
+export const BtnTitle = styled.Text`
+  color: white;
+  letter-spacing: 1px;
+  font-weight: 400;
+  paddingHorizontal: 14px;
+  paddingVertical: 5px;
 `
